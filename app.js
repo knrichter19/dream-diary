06886acec976c41654002ab7fc8879fcea3ee376
knrichter19 <knrichter19@coe.edu>
2022-06-19 12:11:18 -0600

    gets and posts dreams from mysql

--- app.js
+++ app.js
@@ -1,39 +1,83 @@
 const express = require("express");
+const mysql = require("mysql");
+require('dotenv').config();
+
+// express setup
 const app = express();
 const expressPort = 3000;
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-app.listen(expressPort, () => {
-    console.log(`listening on port ${expressPort}`)
+// mysql setup
+const con = mysql.createConnection({
+    host: "localhost",
+    user: process.env.user,
+    password: process.env.password,
+    database: "dream_test"
 });
 
+con.connect((err)=>{
+    if (err) throw err;
+    console.log("connected to mysql!");
+    app.listen(expressPort, () => {
+        console.log(`listening on port ${expressPort}`)
+    });
+});
+
+
+// dream endpoints 
 app.get("/dreams", (req, res)=>{
-    res.send("Dream GET endpoint")
+    console.log("Dream GET endpoint")
+    const sql = "SELECT * FROM dreams";
+    con.query(sql, (error, results, fields) =>{
+        if (error){
+            return console.error(error.message);
+        }
+        res.json(JSON.parse(JSON.stringify(results)));
+    })
 });
 
 app.post("/dreams", (req, res)=>{
-    res.send("Dream POST endpoint")
+    let sql = "INSERT INTO dreams (author, dateDreamed, title, body, nightmare, lucid) VALUES (";
+    sql += req.body.author + ","
+    sql += "'" + req.body.dateDreamed + "',"
+    sql += "'" + req.body.title + "',"
+    sql += "'" + req.body.body + "',"
+    sql += req.body.nightmare + ","
+    sql += req.body.lucid + ");"
+    // todo: fix sql injection
+    
+    console.log("Dream POST endpoint")
+    console.log(sql);
+
+    con.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send("works");
+    })
 });
 
 app.get("/dreams/:id", (req, res)=>{
-    res.send(`Dream id=${req.params.id} GET endpoint`)
+    console.log(`Dream id=${req.params.id} GET endpoint`)
 });
 
 app.delete("/dreams/:id", (req, res)=>{
-    res.send(`Dream id=${req.params.id} DELETE endpoint`)
+    console.log(`Dream id=${req.params.id} DELETE endpoint`)
 });
 
+
+// user endpoints
 app.get("/users", (req, res)=>{
-    res.send("Users GET endpoint");
+    console.log("Users GET endpoint");
 });
 
 app.post("/users", (req, res)=>{
-    res.send("Users POST endpoint")
+    console.log("Users POST endpoint")
 });
 
 app.get("/users/:id", (req, res)=>{
-    res.send(`User id=${req.params.id} GET endpoint`)
+    console.log(`User id=${req.params.id} GET endpoint`)
 });
 
 app.get("/users/:id/dreams", (req,res)=>{
-    res.send(`User id=${req.params.id} dream GET endpoint`)
+    console.log(`User id=${req.params.id} dream GET endpoint`)
 });
